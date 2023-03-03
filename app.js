9da20bbe0b6899b6cb86c2b448c05c7eba6cee1f
KR <112645912+kamizatoR@users.noreply.github.com>
2023-03-03 09:06:03 +0000

    pracitce jQuery event

--- app.js
+++ app.js
@@ -28,13 +28,40 @@
 //   $('.box1').hide();
 // })
 
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//     'background-color': '#0000FF',
+//     'width' : '200px',
+//     'height': '100px'
+//     });
+//   });
+//   $('.box1').slideUp();
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-    'background-color': '#0000FF',
-    'width' : '200px',
-    'height': '100px'
-    });
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
   });
-  $('.box1').slideUp();
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
 });
